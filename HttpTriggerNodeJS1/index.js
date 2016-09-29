d8c60a48100a98e323cf41e1d64a1bd430746fa2
Mohamed Ali Abdi <maxali.abdi@gmail.com & echo. & cd>
2016-09-29 13:31:36 +0000

    fix conflict

--- HttpTriggerNodeJS1/index.js
+++ HttpTriggerNodeJS1/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = function(context, req) {
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
 
@@ -15,22 +14,4 @@ module.exports = function(context, req) {
         };
     }
     context.done();
-=======
-module.exports = function(context, req) {
-    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done();
->>>>>>> 07a4897577f933a22832e17d3f670995128aa1fd
 };
